8456d43c207b88f4d57229dd2b05d9776d34d0f7
Dan Milon <i@danmilon.me>
2015-11-13 23:55:19 +0200

    partially fix datetime to timestamp

--- open-csa-wp-db-tables.php
+++ open-csa-wp-db-tables.php
@@ -8,12 +8,12 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 function open_csa_wp_db_tables_creation () {
 
-	global $wpdb; 
+	global $wpdb;
 	$charset_collate = $wpdb->get_charset_collate();
-	
-	
-	$sql = "	
-	
+
+
+	$sql = "
+
 	CREATE TABLE ". OPEN_CSA_WP_TABLE_SPOTS ." (
 		id int(4) NOT NULL UNIQUE AUTO_INCREMENT,
 		spot_name varchar(30) NOT NULL,
@@ -33,17 +33,17 @@ function open_csa_wp_db_tables_creation () {
 		parking enum('easy','possible','hard','impossible') DEFAULT NULL,
 		PRIMARY KEY  (id)
 	) $charset_collate;
-	
+
 	CREATE TABLE ". OPEN_CSA_WP_TABLE_SPOTS_TO_USERS ." (
-		spot_id  int(10) NOT NULL, 
-		user_id  int(10) NOT NULL, 
+		spot_id  int(10) NOT NULL,
+		user_id  int(10) NOT NULL,
 		type enum('production','delivery','home') NOT NULL,
 		PRIMARY KEY  (spot_id,user_id,type)
-	) $charset_collate;	
-	
+	) $charset_collate;
+
 	CREATE TABLE ". OPEN_CSA_WP_TABLE_PRODUCT_CATEGORIES ." (
-		id  int(4) NOT NULL AUTO_INCREMENT, 
-		name varchar(20) NOT NULL, 
+		id  int(4) NOT NULL AUTO_INCREMENT,
+		name varchar(20) NOT NULL,
 		description varchar(100) DEFAULT NULL,
 		PRIMARY KEY  (id)
 	) $charset_collate;
@@ -52,7 +52,7 @@ function open_csa_wp_db_tables_creation () {
 		id int(10) NOT NULL AUTO_INCREMENT,
 		name varchar(30) NOT NULL,
 		category int(4) NOT NULL,
-		variety varchar(30) DEFAULT NULL,		
+		variety varchar(30) DEFAULT NULL,
 		current_price_in_euro float(5) NOT NULL,
 		measurement_unit enum('piece', 'litre', 'kilogram', 'bunch') NOT NULL,
 		producer int(4) NOT NULL,
@@ -60,7 +60,7 @@ function open_csa_wp_db_tables_creation () {
 		is_available boolean NOT NULL,
 		PRIMARY KEY  (id)
 	) $charset_collate;
-	
+
 	CREATE TABLE ". OPEN_CSA_WP_TABLE_DELIVERIES ." (
 		id int(11) NOT NULL AUTO_INCREMENT,
 		spot_id int(4) NOT NULL,
@@ -68,7 +68,7 @@ function open_csa_wp_db_tables_creation () {
 		order_deadline_time time NOT NULL,
 		delivery_date date NOT NULL,
 		delivery_start_time time NOT NULL,
-		delivery_end_time time NOT NULL,		
+		delivery_end_time time NOT NULL,
 		user_in_charge int(4) DEFAULT NULL,
 		are_orders_open boolean NOT NULL,
 		PRIMARY KEY  (id)
@@ -79,24 +79,24 @@ function open_csa_wp_db_tables_creation () {
 		user_id int(11) NOT NULL,
 		product_id int(11) NOT NULL,
 		quantity int(4) NOT NULL,
-		status enum('pending', 'accomplished', 'cancelled') DEFAULT 'pending', 
+		status enum('pending', 'accomplished', 'cancelled') DEFAULT 'pending',
 		custom_price float(5) DEFAULT NULL,
 		comments varchar(100) DEFAULT NULL,
 		submission_or_last_edit_datetime datetime DEFAULT NULL,
 		PRIMARY KEY  (delivery_id,user_id,product_id)
 	) $charset_collate;
-	
+
 	CREATE TABLE ". OPEN_CSA_WP_TABLE_USER_ORDERS ." (
 		delivery_id int(11) NOT NULL,
 		user_id int(11) NOT NULL,
 		time_of_arrival time DEFAULT NULL,
 		comments varchar(500) DEFAULT NULL,
-		submission_datetime datetime DEFAULT NOW(),
+		submission_datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 		last_edit_datetime datetime DEFAULT NULL,
 		PRIMARY KEY  (delivery_id,user_id)
 	) $charset_collate;
 	";
-		
+
 	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 	dbDelta( $sql );
 
@@ -110,7 +110,7 @@ function open_csa_wp_db_tables_creation () {
 
 function open_csa_wp_db_tables_drop() {
 
-	global $wpdb; 
+	global $wpdb;
 
 	$wpdb->query("DROP TABLE IF EXISTS ". OPEN_CSA_WP_TABLE_SPOTS);
 	$wpdb->query("DROP TABLE IF EXISTS ". OPEN_CSA_WP_TABLE_SPOTS_TO_USERS);
